d4d75090d4a6d2c861fc135e3ab72cd193098383
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-04-07 10:38:16 +0200

    Fixni formatovanie casu

--- lib/Candle.php
+++ lib/Candle.php
@@ -3,7 +3,10 @@
 class Candle {
     static public function formatTime($timeVal) {
         $h = intval($timeVal/60);
-        $m = $timeVal%60;
+        $m = ''.$timeVal%60;
+        if (strlen($m)<2) {
+            $m = '0'.$m;
+        }
         return $h.':'.$m;
     }
     
