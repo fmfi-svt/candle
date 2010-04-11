298b45a670b49bb936cc04df89b130150628f6a4
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-04-12 00:02:11 +0200

    Oprav zobrazovanie policok v rozvrhu v malych velkostiach

--- apps/frontend/modules/timetable/templates/_cell.php
+++ apps/frontend/modules/timetable/templates/_cell.php
@@ -1,5 +1,5 @@
 <div class="wrap"><!--
     --><div class="predmet_wrap"><div class="predmet"><?php echo $lesson->getSubject(); ?></div></div><!--
     --><div class="miestnost"><?php echo $lesson->getRoom(); ?></div><!--
-    --><div class="typ"><?php echo $lesson->getLessonType(); ?></div><!--
+    --><div class="typ"><abbr title="<?php echo $lesson->getLessonType(); ?>"><?php echo $lesson->getLessonType()->getCode(); ?></abbr></div><!--
     --><!-- TODO <label class="pristupnost" for="zobrazitHodinu1">Zobraziť Predmet štvrtok 12:20</label><input type="checkbox" class="vybrany" id="zobrazitHodinu1" />--></div>
