88813c9da0b4171aef5657141ed5aa56086e3828
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-04-11 16:53:25 +0200

    Vylepsenie hladania predmetov

--- lib/model/doctrine/SubjectTable.class.php
+++ lib/model/doctrine/SubjectTable.class.php
@@ -11,14 +11,25 @@ class SubjectTable extends Doctrine_Table
         return $q->execute();
     }
     
-    public function searchSubjectsByAll($queryString) {
+    public function searchSubjectsByAll($queryString, $limit = 50) {
+        $matchQueryString = '%'.str_replace(array(' ', '.'), '%', $queryString).'%';
+        
         $q = Doctrine_Query::create()
             ->from('Subject s')
             ->innerJoin('s.Lessons l')
             ->leftJoin('l.Teacher t')
             ->leftJoin('l.LessonType ty')
-            ->leftJoin('l.Room r')
-            ->andWhere('s.name LIKE ?', '%'.$queryString.'%');
+            ->leftJoin('l.Room r');
+        
+        if (strlen($queryString)>2) {
+            $q->andWhere('s.name LIKE ?', $matchQueryString)
+                ->orWhere('s.code LIKE ?', $matchQueryString)
+                ->orWhere('EXISTS (SELECT st2.id FROM Teacher st2 LEFT JOIN st2.Lesson sl2 WHERE CONCAT(st2.given_name,\' \',st2.family_name) LIKE ? AND sl2.subject_id=s.id)', $matchQueryString);
+        }
+        
+        $q->orWhere('EXISTS (SELECT sl.id FROM Lesson sl LEFT JOIN sl.Room sr WHERE sr.name LIKE ? AND sl.subject_id=s.id)', $matchQueryString)
+            ->orderBy('s.name')
+            ->limit($limit);
         return $q->execute();
     }
     
