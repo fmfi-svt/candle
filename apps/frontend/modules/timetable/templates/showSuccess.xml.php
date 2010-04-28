6d3a9c9940921b5aea8b706614a88917e5bab763
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-04-28 21:25:39 +0200

    Pridaj do xml exportu id-cka hodin

--- apps/frontend/modules/timetable/templates/showSuccess.xml.php
+++ apps/frontend/modules/timetable/templates/showSuccess.xml.php
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8" ?>
 <timetable version="0.1" name="<?php echo $timetable->getName(); ?>">
 <?php foreach ($layout->getLessons() as $lesson): ?>
-    <lesson>
+    <lesson id="<?php echo $lesson['id']; ?>">
         <type><?php echo $lesson['LessonType']['name']; ?></type>
         <room><?php echo $lesson['Room']['name']; ?></room>
         <subject><?php echo $lesson['Subject']['name']; ?></subject>
