a501e998379fea45129ce643027910135c18a482
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-04-29 12:01:16 +0200

    Pridaj zakladny editor rozvrhu

--- web/js/timetable_editor.js
+++ web/js/timetable_editor.js
@@ -1,11 +1,64 @@
+var TimetableEditor = new Class({
+    Implements: Options,
+    options: {
+
+    },
+    initialize: function(timetableForm, options) {
+        this.setOptions(options);
+        this.timetableForm = $(timetableForm);
+        this.bindCheckboxes();
+    },
+    timetableForm: null,
+    bindCheckboxes: function() {
+        // checboxy na upravu pritomnosti v rozvrhu
+        // zatial treba poslat request a loadnut cely rozvrh zo servera
+        this.timetableForm.getElements('input[type=checkbox][name="lesson[]"]')
+                .each(function(checkbox) {
+            checkbox.addEvent('change', this.lessonCheckboxChanged.create({
+                event: true,
+                bind: this
+            }));
+        }, this);
+        // checkboxy na upravu zvyraznenia
+        this.timetableForm.getElements('input[type=checkbox][name="lessonHighlighted[]"]')
+                .each(function(checkbox) {
+            checkbox.addEvent('change', this.lessonHighlightedCheckboxChanged.create({
+                event: true,
+                bind: this
+            }));
+        }, this);
+    },
+    refreshTimetable: function() {
+        this.timetableForm.submit();
+    },
+    lessonCheckboxChanged: function(event) {
+        this.refreshTimetable();
+    },
+    lessonHighlightedCheckboxChanged: function(event) {
+        var checkbox = $(event.target);
+        var cell = this.getTimetableCell(checkbox);
+        if (checkbox.checked) {
+            cell.addClass('highlighted');
+        }
+        else {
+            cell.removeClass('highlighted');
+        }
+    },
+    getTimetableCell: function(el) {
+        // najdi bunku pre tento element (on sam, alebo najblizsi rodic typu td)
+        var element = $(el);
+        if (element.get('tag') == 'td') return element;
+        return element.getParent('td');
+    }
+
+});
+
 window.addEvent('domready', function() {
 
   $$("#panel_change_lessons input[type=checkbox]").addEvent('change', function() {
       $('panel_change_lessons').submit();
   });
 
-  $$("#timetable_editor_form input[type=checkbox]").addEvent('change', function() {
-      $('timetable_editor_form').submit();
-  });
+  new TimetableEditor('timetable_editor_form');
 
 });
