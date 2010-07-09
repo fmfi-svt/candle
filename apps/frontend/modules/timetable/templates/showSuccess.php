95e4c62c550843c204baf628adb798230f45078e
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-07-09 21:49:58 +0200

    Pridany zaklad novych ovladacich prvkov na upravu tabulky rozvrhu
    Len jeden checkbox v bunke tabulky pravdepodobne zvysi pouzitelnost rozvrhu pre novych pouzivatelov

--- apps/frontend/modules/timetable/templates/showSuccess.php
+++ apps/frontend/modules/timetable/templates/showSuccess.php
@@ -16,6 +16,23 @@ if (!$sf_request->isXmlHttpRequest()) {
 ?>
 <div id="timetable_editor">
 <?php echo form_tag('@timetable_change_lessons?id='.$timetable_id, array('id'=>'timetable_editor_form')); ?><div>
+
+<div id="timetable_editor_command_bar">
+
+Všetky
+<select name="selection_source" size="1">
+    <option value="selection" id="selection_source_selection">vybrané</option>
+    <option value="highlight" id="selection_source_highlight">zvýraznené</option>
+</select>
+
+hodiny v rozvrhu
+
+<button name="selection_action" type="submit">Odstrániť</button>
+<button name="selection_action" type="highlight">Zvýrazňovať</button>
+<button name="selection_action" type="unhighlight">Nezvýrazňovať</button>
+
+</div> <!-- timetable_editor_command_bar -->
+
 <?php include_partial('timetable/table', array('timetable'=>$timetable, 'layout'=>$layout, 'editable'=>true)); ?>
 </div>
 <div>
