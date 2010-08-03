d8cdc886b3bf0380605a56f6e7167465223bf7b9
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-08-03 12:43:26 +0200

    Vypis hlasku ak sa dosiahol limit vysledkov vyhladavania

--- apps/frontend/modules/panel/templates/_list_lessons.php
+++ apps/frontend/modules/panel/templates/_list_lessons.php
@@ -93,6 +93,13 @@ if ($timetable) {
 
 </ul>
 
+<?php if (count($subjects) == 50) {
+    // TODO toto by mohlo z DB zistit, ci sa prekrocil alebo len dosiahol
+    // tento limit + limit by sa mal dat nastavovat
+    echo '<div>Výsledky vyhľadávania limitované na 50 výsledkov, skúste zadať presnejšie kritériá</div>';
+}
+?>
+
 <?php if ($timetable): ?>
 <button type="submit" class="jshide">Zmeniť hodiny</button>
 <?php endif ?>
--- apps/frontend/modules/panel/templates/_list_teachers.php
+++ apps/frontend/modules/panel/templates/_list_teachers.php
@@ -7,3 +7,10 @@
 <?php endforeach; ?>
 
 </ul>
+
+<?php if (count($teachers) == 50) {
+    // TODO toto by mohlo z DB zistit, ci sa prekrocil alebo len dosiahol
+    // tento limit + limit by sa mal dat nastavovat
+    echo '<div>Výsledky vyhľadávania limitované na 50 výsledkov, skúste zadať presnejšie kritériá</div>';
+}
+?>
