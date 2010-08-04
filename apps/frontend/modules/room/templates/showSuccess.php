3aa67694c91059e5c456385536864add0397bcee
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-08-04 15:25:19 +0200

    Zobraz dalsie dostupne informacie o miestnosti

--- apps/frontend/modules/room/templates/showSuccess.php
+++ apps/frontend/modules/room/templates/showSuccess.php
@@ -14,6 +14,11 @@ end_slot();
 ?>
 <h1 class="posunuty"><?php echo $room['name']; ?></h1>
 
+<p>
+    Typ miestnosti: <?php echo $room['RoomType']['name']; ?><br />
+    Kapacita: <?php echo $room['capacity']; ?><br />
+</p>
+
 <div>
 <?php include_partial('timetable/table',
         array(  'timetable'=>$timetable,
