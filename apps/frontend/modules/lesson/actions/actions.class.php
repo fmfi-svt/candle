9672a060d84c6457d8144f1f04efc9a19c97e88d
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-08-29 22:50:16 +0200

    Pridane copyrightove a licencne hlavicky do vacsiny podstatnych suborov a tiez cely text GPLv3
    Fixes Issue #22

--- apps/frontend/modules/lesson/actions/actions.class.php
+++ apps/frontend/modules/lesson/actions/actions.class.php
@@ -1,12 +1,29 @@
 <?php
 
 /**
+
+    Copyright 2010 Martin Sucha
+
+    This file is part of Candle.
+
+    Candle is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Candle is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Candle.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
  * lesson actions.
  *
- * @package    candle
- * @subpackage lesson
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class lessonActions extends sfActions
 {
