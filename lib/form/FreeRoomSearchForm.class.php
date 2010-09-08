9e79926a8d8c432030a7ab98afb0a12d8d5ae206
Martin Sucha <sucha14@st.fmph.uniba.sk>
2010-09-08 13:39:22 +0200

    Vylepseny help k intervalom

--- lib/form/FreeRoomSearchForm.class.php
+++ lib/form/FreeRoomSearchForm.class.php
@@ -46,8 +46,28 @@ class FreeRoomSearchForm extends sfForm {
         ));
 
         $this->widgetSchema->setHelps(array(
-            'requiredAmount' => 'Aká musí byť minimálna dĺžka voľného okna v miestnosti (t.j. napríklad dĺžka hodiny, ktorú by ste chceli presunúť). Hodnoty sa dajú zadať v tvare <span class="example">1:35</span>, <span class="example">1h 35m</span>, <span class="example">1h</span>, <span class="example">35m</span> alebo <span class="example">2 v.h</span>',
-            'searchIntervals' => 'Časové intervaly, na ktoré obmedziť vyhľadávanie. Tu sa zadáva čiarkami oddelený zoznam intervalov, v ktorých hľadať (hľadá sa v ich zjednotení). Jednotlivé intervaly sú zadávané vo formáte <span class="example">Po 16:30-18:40</span> alebo <span class="example">Po 16:30-Ut 7:00</span> (dni môžu byť prípadne aj rozpísané ako v <span class="example">Pondelok 16:30-Utorok 12:00</span>)',
+            'requiredAmount' => 'Aká musí byť minimálna dĺžka voľného okna v miestnosti 
+                (t.j. napríklad dĺžka hodiny, ktorú by ste chceli presunúť).
+                Hodnoty sa dajú zadať v tvare <span class="example">1:35</span>,
+                <span class="example">1h 35m</span>,
+                <span class="example">1h</span>,
+                <span class="example">35m</span> alebo
+                <span class="example">2 v.h</span>',
+            'searchIntervals' => 'Časové intervaly, na ktoré obmedziť vyhľadávanie. 
+                Tu sa zadáva čiarkami oddelený zoznam intervalov,
+                v ktorých hľadať (hľadá sa v ich zjednotení).
+                Jednotlivé intervaly sú zadávané vo formáte
+                <span class="example">Po 16:30-18:40</span> alebo
+                <span class="example">Po 16:30-Ut 7:00</span>
+                (dni môžu byť prípadne aj rozpísané ako v
+                <span class="example">Pondelok 16:30-Utorok 12:00</span>).
+                Pri zadávaní viacerých intervalov je možné vynechať názov dňa
+                v prípade, že už bol špecifikovaný, napr.
+                <span class="example">Po 16:30-18:00, 19:00-20:00</span>.
+                Túto možnosť nie je možné využiť, ak je špecifikovaný aj
+                druhý deň intervalu, pretože by vstup nebol jednoznačný (t.j.
+                nasledovné vyhodí chybu:
+                <span class="bad example">Po 16:30-Ut 18:00, 19:00-20:00</span>)',
         ));
 
         $this->disableLocalCSRFProtection();
