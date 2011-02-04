92e3fc8566a2c9b67f86171c331b650a70bf041f
Martin Sucha <sucha14@st.fmph.uniba.sk>
2011-02-04 15:01:54 +0100

    Zobrazovanie copyright pri rozvrhu z posledneho datumu aktualizacie

--- apps/frontend/modules/layout/templates/_lastUpdate.php
+++ apps/frontend/modules/layout/templates/_lastUpdate.php
@@ -6,7 +6,7 @@ Posledná aktualizácia rozvrhovej databázy:
     if (!empty($lastUpdate['description'])):
         echo ' ('.$lastUpdate['description'].')';
     endif;
-    echo ' Rozvrhové dáta Copyright 2010 Rozvrhári FMFI UK.';
+    echo ' Rozvrhové dáta Copyright '.substr($lastUpdate['datetime'],0,4).' Rozvrhári FMFI UK.';
 else:
     echo 'žiadna (pravdepodobne nebol vykonaný žiaden import dát)';
 endif;
