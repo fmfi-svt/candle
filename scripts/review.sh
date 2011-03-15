d85753a129daae5d46b1294b566b5f76207d8900
Martin Sucha <sucha14@st.fmph.uniba.sk>
2011-03-15 20:21:12 +0100

    Fixnuty upload, aby spustal upload.py z rootu (inac nefungoval upload suborov mimo scripts)

--- scripts/review.sh
+++ scripts/review.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd "`dirname $0`"
-./upload.py --cc "candle-devel+codereview@googlegroups.com" --base_url "http://ho.st.dcs.fmph.uniba.sk/~mato/hg/candle" $@
+cd "`dirname $0`/.."
+./scripts/upload.py --cc "candle-devel+codereview@googlegroups.com" --base_url "http://ho.st.dcs.fmph.uniba.sk/~mato/hg/candle" $@
 
