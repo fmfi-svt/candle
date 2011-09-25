eda1288180ce316cc543bc05d61e18da003fcfb4
Tomi Belan <tomi.belan@gmail.com>
2011-09-25 12:32:52 +0200

    prepisany a bugfixnuty style switcher.

--- web/js/styles.js
+++ web/js/styles.js
@@ -1,84 +1,32 @@
 
 (function () {
 
-// from http://www.alistapart.com/articles/alternate/
-// with some modifications
-
 function setActiveStyleSheet(title) {
-  var i, a, main;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
-      a.disabled = true;
-      if(a.getAttribute("title") == title) a.disabled = false;
-    }
+  var styles = $$('link[rel*="style"][title]');
+  if (styles.filter(function (s) { return s.title == title; }).length != 0) {
+    styles.each(function (s) { s.disabled = (s.title != title); });
   }
 }
 
 function getActiveStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
-  }
-  return null;
+  var enabled = $$('link[rel*="style"][title]:enabled');
+  return enabled.length ? enabled[0].title : null;
 }
 
-function getPreferredStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1
-       && a.getAttribute("rel").indexOf("alt") == -1
-       && a.getAttribute("title")
-       ) return a.getAttribute("title");
-  }
-  return null;
-}
+setActiveStyleSheet(Cookie.read("candle_style"));
 
-function createCookie(name,value,days) {
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
-  }
-  else expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
-// end of ALA code
-
-setActiveStyleSheet(readCookie("candle_style") || getPreferredStyleSheet());
-
-// addEvent is mootools-specific
 window.addEvent('unload', function () {
-  createCookie("candle_style", getActiveStyleSheet(), 365);
+  Cookie.write("candle_style", getActiveStyleSheet(), { duration: 365 });
 });
 
 function clickedSwitchButton() {
-  var stylesheets = [];
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('title')) {
-      stylesheets.push(a);
-    }
-  }
-
-  for(var i = 0; i < stylesheets.length; i++) {
-    if(!stylesheets[i].disabled) {
-      setActiveStyleSheet(stylesheets[(i + 1) % stylesheets.length].getAttribute('title'));
+  var styles = $$('link[rel*="style"][title]');
+  for (var i = 0; i < styles.length; i++) {
+    if (!styles[i].disabled) {
+      setActiveStyleSheet(styles[(i+1) % styles.length].title);
       break;
     }
   }
-
   return false;
 }
 
