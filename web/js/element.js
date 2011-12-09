1bc52a72d1b0cf8eccad91bb73dd24006101fa19
Martin Sucha <sucha14@st.fmph.uniba.sk>
2011-12-09 13:08:40 +0100

    Revertnute zmeny v replaceWith pretoze sposobovali problemy v Chrome.

--- web/js/element.js
+++ web/js/element.js
@@ -4,10 +4,16 @@ Element.implement({
         if (!Browser.Engine.webkit) return;
         this.replaceWith(this);
     },
-    replaceWith: function(newElement) {
-        var parentNode = $(this.parentNode);
-        parentNode.insertAfter(newElement, this);
+    replaceWith: function(other) {
+        var parentNode = this.parentNode;
+        var nextSibling = this.nextSibling;
         parentNode.removeChild(this);
+        if (nextSibling) {
+            parentNode.insertBefore(other, nextSibling);
+        }
+        else {
+            parentNode.appendChild(other);
+        }
     },
     insertAfter: function(newElement, referenceElement) {
         var nextSibling = referenceElement.nextSibling;
