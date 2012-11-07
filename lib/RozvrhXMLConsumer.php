d9f05884b961a17612dd646491475902f9f9e522
Martin Sucha <sucha14@uniba.sk>
2012-11-07 16:15:57 +0100

    Pridana dokumentacia interface RozvrhXMLConsumer

--- lib/RozvrhXMLConsumer.php
+++ lib/RozvrhXMLConsumer.php
@@ -22,16 +22,150 @@
 */
 
 /**
- *
+ * Interface for consumers of parse events from RozvrhXMLParser
  */
 
 interface RozvrhXMLConsumer
 {
+    /**
+     * @param array $location current location within XML
+     *              array(
+     *                  'line' => integer,
+     *                  'column' => integer,
+     *                  'byte_offset' => integer,
+     *                  'element_path' => array(string),
+     *              )
+     *              see RozvrhXMLParser->getCurrentLocation()
+     * @param array $data
+     *              array(
+     *                  'verzia' => integer unix time,
+     *                  'semester' => string,
+     *                  'skolrok' => string,
+     *              )
+     */
     public function consumeRozvrh(array $location, array $data);
+    
+    /**
+     * @param array $location current location within XML
+     *              array(
+     *                  'line' => integer,
+     *                  'column' => integer,
+     *                  'byte_offset' => integer,
+     *                  'element_path' => array(string),
+     *              )
+     *              see RozvrhXMLParser->getCurrentLocation()
+     * @param array $data
+     *              array(
+     *                  'id' => string,
+     *                  'popis' => string,
+     *              )
+     */
     public function consumeTypHodiny(array $location, array $data);
+    
+    /**
+     * @param array $location current location within XML
+     *              array(
+     *                  'line' => integer,
+     *                  'column' => integer,
+     *                  'byte_offset' => integer,
+     *                  'element_path' => array(string),
+     *              )
+     *              see RozvrhXMLParser->getCurrentLocation()
+     * @param array $data
+     *              array(
+     *                  'id' => string,
+     *                  'popis' => string,
+     *              )
+     */
     public function consumeTypMiestnosti(array $location, array $data);
+    
+    /**
+     * @param array $location current location within XML
+     *              array(
+     *                  'line' => integer,
+     *                  'column' => integer,
+     *                  'byte_offset' => integer,
+     *                  'element_path' => array(string),
+     *              )
+     *              see RozvrhXMLParser->getCurrentLocation()
+     * @param array $data
+     *              array(
+     *                  'nazov' => string,
+     *                  'kapacita' => string,
+     *                  'typ' => string,
+     *              )
+     */
     public function consumeMiestnost(array $location, array $data);
+    
+    /**
+     * @param array $location current location within XML
+     *              array(
+     *                  'line' => integer,
+     *                  'column' => integer,
+     *                  'byte_offset' => integer,
+     *                  'element_path' => array(string),
+     *              )
+     *              see RozvrhXMLParser->getCurrentLocation()
+     * @param array $data
+     *              array(
+     *                  'id' => string,
+     *                  'priezvisko' => string,
+     *                  'meno' => string,
+     *                  'iniciala' => string,
+     *                  'katedra' => string,
+     *                  'oddelenie' => string,
+     *                  'login' => string,
+     *              )
+     */
     public function consumeUcitel(array $location, array $data);
+    
+    /**
+     * @param array $location current location within XML
+     *              array(
+     *                  'line' => integer,
+     *                  'column' => integer,
+     *                  'byte_offset' => integer,
+     *                  'element_path' => array(string),
+     *              )
+     *              see RozvrhXMLParser->getCurrentLocation()
+     * @param array $data
+     *              array(
+     *                  'id' => string,
+     *                  'nazov' => string,
+     *                  'kod' => string,
+     *                  'kratkykod' => string,
+     *                  'kredity' => string,
+     *                  'rozsah' => string,
+     *              )
+     */
     public function consumePredmet(array $location, array $data);
+    
+    /**
+     * @param array $location current location within XML
+     *              array(
+     *                  'line' => integer,
+     *                  'column' => integer,
+     *                  'byte_offset' => integer,
+     *                  'element_path' => array(string),
+     *              )
+     *              see RozvrhXMLParser->getCurrentLocation()
+     * @param array $data
+     *              array(
+     *                  'id' => string,
+     *                  'den' => string,
+     *                  'zaciatok' => string,
+     *                  'koniec' => string,
+     *                  'miestnost' => string,
+     *                  'trvanie' => string,
+     *                  'predmet' => string,
+     *                  'ucitelia' => string,
+     *                  'kruzky' => string,
+     *                  'typ' => string,
+     *                  'zviazanehodiny' => string,
+     *                  'oldid' => string,
+     *                  'zviazanieoldid' => string,
+     *                  'poznamka' => string,
+     *              )
+     */
     public function consumeHodina(array $location, array $data);
 }
